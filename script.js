aa9e93b65e31206d9ca462280fcb7dbac7610238
Cheta008 <destinychukwuma8@gmail.com>
2024-01-15 14:38:11 -0800

    Added weather functionalities

--- script.js
+++ script.js
@@ -2,8 +2,18 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let descriptionElement = document.querySelector("#description")
+  let iconElement = document.querySelector("#icon")
+
+  
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km\h`;
+  descriptionElement.innerHTML= response.data.condition.description;
+  iconElement.innerHTML = `img src="${response.data.condition.icon_url}" class="current-temperature-icon`;
 }
 
 function search(event) {
@@ -11,7 +21,7 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "fbeat7a34efb4a7e60ae9e40fbb3ccoa";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
@@ -51,3 +61,5 @@ let currentDateELement = document.querySelector("#current-date");
 let currentDate = new Date();
 
 currentDateELement.innerHTML = formatDate(currentDate);
+
+searchInput("Abuja");
